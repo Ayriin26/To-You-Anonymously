5fe3ef9a3f2847e9d91a00cb835c131ae647dd04
arin3949 <arinbisht46@gmail.com>
2025-06-17 20:52:12 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://to-you-anonymously-backend.onrender.com';
+
 
 // DOM Elements
 const addNoteBtn = document.getElementById('addNoteBtn');
